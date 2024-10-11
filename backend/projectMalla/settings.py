07f8168a76eb5c70331b4d9185eb7208c3daf107
vitor <victor.2002.espinoza@gmail.com>
2024-10-10 23:17:58 -0300

    reestructurando

--- backend/projectMalla/settings.py
+++ backend/projectMalla/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Ajusta según tu frontend
+    "http://localhost:5173",  # Ajusta según tu frontend
 ]
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,13 @@ import SubjectList from './components/SubjectList';
 import SubjectForm from './components/SubjectForm';
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Gestión de Asignaturas</h1>
-            <SubjectForm />
-            <SubjectList />
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Gestión de Asignaturas</h1>
+      <SubjectForm />
+      <SubjectList />
+    </div>
+  );
 }
 
 export default App;
