467c9d2ac51caed84c056558df48a785b0b91103
vitor <victor.2002.espinoza@gmail.com>
2024-10-11 01:41:40 -0300

    Cambios piolas

--- frontend/src/components/MallaCurricular.jsx
+++ frontend/src/components/MallaCurricular.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './MallaCurricular.css'; // Asegúrate de que esta ruta es correcta
 
 const MallaCurricular = () => {
   const [asignaturas, setAsignaturas] = useState({});
@@ -16,18 +17,41 @@ const MallaCurricular = () => {
 
   return (
     <div className="malla-curricular">
-      {Object.keys(asignaturas).map(semestre => (
-        <div key={semestre} className="semestre-columna">
-          <h3>Semestre {semestre}</h3>
-          <ul>
-            {asignaturas[semestre].map(asignatura => (
-              <li key={asignatura.id}>
-                {asignatura.nombre} ({asignatura.creditos} créditos)
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <div className="malla-container">
+        {Object.keys(asignaturas).map(semestre => {
+          const asignaturasSemestre = asignaturas[semestre];
+
+          // Filtrar prácticas
+          const practicas = asignaturasSemestre.filter(asignatura => asignatura.nombre.includes('Práctica'));
+          const asignaturasSinPracticas = asignaturasSemestre.filter(asignatura => !asignatura.nombre.includes('Práctica'));
+
+          return (
+            <div key={semestre} className="semestre-columna">
+              <h3>Semestre {semestre}</h3>
+              <div className="contenido-semestre">
+                {practicas.length > 0 && (
+                  <div className="practica-columna">
+                    {practicas.map(practica => (
+                      <ul key={practica.id} >
+                        {practica.nombre} ({practica.creditos} créditos)
+                      </ul>
+                    ))}
+                  </div>
+                )}
+                <div className="asignaturas-columna">
+                  <ul>
+                    {asignaturasSinPracticas.map(asignatura => (
+                      <li key={asignatura.id} className="cuadro">
+                        {asignatura.nombre} ({asignatura.creditos} créditos)
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
